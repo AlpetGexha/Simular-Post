4aedc02ea579b3ab446102487c65831a0dcb900f
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-05-23 21:38:59 +0200

    Add People u May Know
    Query will give you a stronger "People You May Know" list that considers both the exclusion of already followed users and the count of shared reactions as indicators of potential connections.

---
+++ app/Http/Controllers/PeopleMayKnowController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PeopleMayKnowController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $userId = auth()->user()->id;
+
+        // $peopleYouMayKnow = User::select('users.*')
+        //     ->leftJoin('followables', function ($join) use ($userId) {
+        //         $join->on('users.id', '=', 'followables.followable_id')
+        //             ->where('followables.user_id', $userId);
+        //     })
+        //     ->whereNull('followables.id')
+        //     ->where('users.id', '<>', $userId)
+        //     ->orderBy('users.created_at', 'desc')
+        //     ->take(10)
+        //     ->get();
+
+        // $peopleYouMayKnow contains the "People You May Know" based on shared interests and not already followed by the authenticated user
+
+        $peopleYouMayKnow = User::query()
+            ->peopleYouMayKnow()
+            ->take(10)
+            ->get();
+
+        // return $peopleYouMayKnow;
+
+        return view('people', compact('peopleYouMayKnow'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PeopleMayKnowController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
@@ -8,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::get('/', PostController::class)->name('home');
     Route::get('users', UserController::class)->name('users.index');
+    Route::get('peopmemayknow', PeopleMayKnowController::class)->name('users.mayknow');
 
     Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
